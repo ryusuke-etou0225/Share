1c050541d28bb428c7b0b82afffbe704f77b1d38
ryusuke.etou <ryusuke.etou.1193@gmail.com>
2025-07-31 15:38:55 +0900

    [Add] java build

--- Share/my-spring-app/src/main/java/com/example/demo/controller/TestController.java
+++ Share/my-spring-app/src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
     public String testEndpoint() {
+        logger.info("アクセスされました");
         return "Hello from /test endpoint!";
     }
 }
